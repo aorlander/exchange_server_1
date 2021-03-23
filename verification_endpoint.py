645e6e1068c7d8aeff63bcf571aba50a168164f5
aorlander <aborlander@gmail.com>
2021-03-22 20:51:39 -0400

    s

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -17,14 +17,14 @@ def verify():
     sig = content['sig']
     payload = content['payload']
     response = False
-
+    
     if platform=='Ethereum':
         eth_encoded_msg = eth_account.messages.encode_defunct(text=payload)
         if eth_account.Account.recover_message(eth_encoded_msg,signature=sig) == pk:
             response = True
 
     if platform=='Algorand':
-        if algosdk.util.verify_bytes(payload,sig,pk):
+        if algosdk.util.verify_bytes(json.dumps(payload).encode('utf-8'),sig,pk):
             response = True
 
     return jsonify(response)
